6f01c54b13531de0d2b2eafce80b846e24f9b9ef
Owen Mo <mochangheng@gmail.com>
2022-05-04 01:19:56 -0400

    Fixed correctness

--- src/lstm_par3.cpp
+++ src/lstm_par3.cpp
@@ -12,8 +12,7 @@
 
 // Set this flag to use cuda
 const bool use_cuda = false;
-int* progress3;
-std::atomic_int* depencies;
+std::atomic_int* progress3;
 
 struct layerComparator
 {
@@ -30,6 +29,7 @@ class PQ
     public:
         int get_layer_task();
         void add_layer_task(int layer, int priority);
+        void check_max_priority(int priority);
         PQ(int max_priority);
     private:
         std::priority_queue<std::tuple<int, int>, std::vector<std::tuple<int, int>>, layerComparator> queue;
@@ -49,7 +49,7 @@ int PQ::get_layer_task(void) {
     // Wait for lock
     while (!success) {
         bool val = false;
-        bool success = pq_busy.compare_exchange_weak(val, true);
+        success = pq_busy.compare_exchange_weak(val, true);
     }
     if (finished) {
         pq_busy = false;
@@ -62,8 +62,7 @@ int PQ::get_layer_task(void) {
     std::tuple<int, int> tuple1 = queue.top(); queue.pop();
     int layer = std::get<0>(tuple1);
     int priority = std::get<1>(tuple1);
-    if (priority >= max_priority)
-        finished = true;
+    check_max_priority(priority);
     pq_busy = false;
     return layer;
 }
@@ -73,10 +72,15 @@ void PQ::add_layer_task(int layer, int priority) {
     // Wait for lock
     while (!success) {
         bool val = false;
-        bool success = pq_busy.compare_exchange_weak(val, true);
+        success = pq_busy.compare_exchange_weak(val, true);
     }
     queue.push(std::make_tuple(layer, priority));
     pq_busy = false;
+}
+
+void PQ::check_max_priority(int priority) {
+    if(priority >= max_priority)
+        finished = true;
 }
 
 template <class Matrix>
@@ -86,6 +90,7 @@ struct ThreadArgs {
     std::vector<Matrix>* hs;
     std::vector<Matrix>* cs;
     LSTM<Matrix>* lstm;
+    int thread_idx;
 };
 
 template <class Matrix>
@@ -102,6 +107,7 @@ void* thread_fn3(void* args) {
     std::vector<Matrix>* hs = thread_args->hs;
     std::vector<Matrix>* cs = thread_args->cs;
     LSTM<Matrix>* lstm = thread_args->lstm;
+    int thread_idx = thread_args->thread_idx;
 
     int input_length = outputs->size();
 
@@ -113,29 +119,29 @@ void* thread_fn3(void* args) {
         }
         if(layer_idx == -2)
             break;
-        lstm->lstm_cells[layer_idx].forward((*outputs)[progress3[layer_idx]], (*hs)[layer_idx], (*cs)[layer_idx], (*hs)[layer_idx], (*cs)[layer_idx]);
-        (*outputs)[progress3[layer_idx]] = (*hs)[layer_idx];
-        progress3[layer_idx]++;
+        lstm->lstm_cells[layer_idx].forward((*outputs)[progress3[layer_idx].load(std::memory_order_relaxed)], (*hs)[layer_idx], (*cs)[layer_idx], (*hs)[layer_idx], (*cs)[layer_idx]);
+        (*outputs)[progress3[layer_idx].load(std::memory_order_relaxed)] = (*hs)[layer_idx];
+        int progress_old_layer = ++progress3[layer_idx];
 
         int old_layer = layer_idx;
         layer_idx = -1;
         int next_layer = old_layer + 1;
         if (next_layer < lstm->depth) {
-            int num_depend = --depencies[next_layer];
-            if (num_depend == 0) {
+            int progress_next_layer = progress3[next_layer].load(std::memory_order_relaxed);
+            if (progress_next_layer + 1 >= progress_old_layer) {
                 layer_idx = next_layer;
-                depencies[next_layer] = 2;
+                pq->check_max_priority(next_layer + progress_next_layer);
             }
         }
-        if (progress3[old_layer] < input_length) {
-            int num_depend = --depencies[old_layer];
-            if (num_depend == 0) {
-                // Layer 0 only has 1 dependency
-                depencies[old_layer] = old_layer == 0 ? 1 : 2;
+        if (progress3[old_layer].load(std::memory_order_relaxed) < input_length) {
+            int progress_below_layer = old_layer == 0 ? input_length : progress3[old_layer - 1].load(std::memory_order_relaxed);
+            if (progress_below_layer >= progress_old_layer + 1) {
+                int priority = old_layer + progress_old_layer;
                 if (layer_idx == -1) {
                     layer_idx = old_layer;
+                    pq->check_max_priority(priority);
                 } else {
-                    pq->add_layer_task(old_layer, old_layer + progress3[old_layer]);
+                    pq->add_layer_task(old_layer, priority);
                 }
             }
         }
@@ -152,12 +158,10 @@ template <class Matrix>
 void LSTM<Matrix>::forward_par3(const std::vector<Matrix>& inputs, Matrix& output, int num_threads) {
     int input_length = inputs.size();
 
-    progress3 = new int[depth];
-    depencies = new std::atomic_int[depth];
+    progress3 = new std::atomic_int[depth];
 
     for (int i = 0; i < depth; i++) {
         progress3[i] = 0;
-        depencies[i] = 1;
     }
 
     std::vector<Matrix> hs;
@@ -172,23 +176,41 @@ void LSTM<Matrix>::forward_par3(const std::vector<Matrix>& inputs, Matrix& outpu
         cs.push_back(c);
     }
 
-    struct ThreadArgs<Matrix> args;
+    //struct ThreadArgs<Matrix> args;
+    struct ThreadArgs<Matrix> args[num_threads];
     pthread_t threads[num_threads];
 
     PQ pq = PQ(depth + input_length - 2);
     pq.add_layer_task(0, 0);
 
-    args.pq = &pq;
-    args.outputs = &outputs;
-    args.hs = &hs;
-    args.cs = &cs;
-    args.lstm = this;
+    //args.pq = &pq;
+    // args.outputs = &outputs;
+    // args.hs = &hs;
+    // args.cs = &cs;
+    // args.lstm = this;
 
     for (int thread_idx = 1; thread_idx < num_threads; thread_idx++) {
-        pthread_create(&threads[thread_idx], NULL, thread_fn3<Matrix>, (void *)(&args));
+        args[thread_idx].pq = &pq;
+        args[thread_idx].outputs = &outputs;
+        args[thread_idx].hs = &hs;
+        args[thread_idx].cs = &cs;
+        args[thread_idx].lstm = this;
+        args[thread_idx].thread_idx = thread_idx;
+        pthread_create(&threads[thread_idx], NULL, thread_fn3<Matrix>, (void *)(&args[thread_idx]));
     }
 
-    thread_fn3<Matrix>((void *)(&args));
+    // for (int thread_idx = 1; thread_idx < num_threads; thread_idx++) {
+    //     pthread_create(&threads[thread_idx], NULL, thread_fn3<Matrix>, (void *)(&args));
+    // }
+
+    args[0].pq = &pq;
+    args[0].outputs = &outputs;
+    args[0].hs = &hs;
+    args[0].cs = &cs;
+    args[0].lstm = this;
+    args[0].thread_idx = 0;
+
+    thread_fn3<Matrix>((void *)(&args[0]));
 
     for (int thread_idx = 1; thread_idx < num_threads; thread_idx++) {
         pthread_join(threads[thread_idx], NULL);
@@ -199,7 +221,6 @@ void LSTM<Matrix>::forward_par3(const std::vector<Matrix>& inputs, Matrix& outpu
 
     // Free resources
     delete[] progress3;
-    delete[] depencies;
 }
 
 template class LSTM<EigenMatrix>;
